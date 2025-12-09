ac53d823afeb6b011bf1dcd0e56ed7b9fd70cc0e
lendellgarcellano <joselendell@gmail.com>
2025-12-09 09:11:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ document.getElementById("generateBtn").addEventListener("click", async () => {
   outputContainer.classList.remove("hidden");
 
   try {
-    const response = await fetch("https://lendell-garcellano.app.n8n.cloud/webhook/portfolio-ai", {
+    const response = await fetch("https://lendell-garcellano.app.n8n.cloud/webhook-test/portfolio-ai", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
